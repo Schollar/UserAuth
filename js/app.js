224fdb98e3da2bf17fb1fe0f4fd3d8ebdd164da9
schollar <cschollar22@gmail.com>
2021-11-27 14:39:29 -0700

    Added Comments

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
+
+// Sets the cookie equal to our response.data.token and then sends user to next page
 function login_success(response) {
-    console.log(response);
     Cookies.set('login_token', response.data.token);
     window.location = "/pages/home.html"
 }
+// Gets called if any errors with API call happen
 function login_failure(error) {
     var error_message = document.getElementById('error_message');
     error_message.innerText = "You did not enter valid Email or Password, please try again."
 }
+// When user attempts to login, we grab the email and password values they have input, and send those values off as data in our POST request, if all good it calls our 
+// login_success function and if not our login_fail function
 function attempt_login(e) {
     var email_input = document.getElementById('email_input').value;
     var password_input = document.getElementById('password_input').value;
@@ -20,7 +24,7 @@ function attempt_login(e) {
         }
     }).then(login_success).catch(login_failure);
 }
+// Setting up parent variables and adding a click listenter to our button that calls the attempt_login button
 var user_section = document.getElementById('user_container');
-
 var login_button = document.getElementById('login_submit');
 login_button.addEventListener('click', attempt_login);
--- js/home.js
+++ js/home.js
@@ -1,5 +1,9 @@
+
+// Function that Gets called when API call succeeds, it creates a color card for all the response data received, loops through and displays each one on the page
 function get_colors_success(response) {
+    // Create Our Parent Card section
     var color_card = document.createElement('section');
+    // Loop through the data received from the API, and display the relevent info on the page
     for (var i = 0; i < response.data.data.length; i++) {
         var color_name = document.createElement('h2');
         color_name.innerText = response.data.data[i].name;
@@ -11,7 +15,7 @@ function get_colors_success(response) {
         color_box.style.width = '200px';
         color_box.style.height = '200px';
         color_box.style.backgroundColor = response.data.data[i].color;
-
+        // Appending the children to the parent so they are seen on page
         color_card.appendChild(color_name);
         color_card.appendChild(color_year);
         color_card.appendChild(color_box);
@@ -19,32 +23,34 @@ function get_colors_success(response) {
     }
 
 }
-
+// Simple function that removes any login token cookies and sends user back to login page
 function logout() {
     Cookies.remove('login_token');
     window.location = "../index.html"
 }
-
+// Error function for if the API call fails
 function get_colors_fail(error) {
     welcome_message.innerText = "Uh-Oh something went wrong, resfresh and try again!"
 }
-
+// Setting up various variables we need to be used as parents
 var welcome_message = document.getElementById('welcome_message');
 var user_token = Cookies.get('login_token');
 var error_message = document.getElementById('error_message');
+// adding a logout button on to the page
 var logout_button = document.createElement('button');
 logout_button.innerText = 'Logout';
+// If button clicked, call the logout function
 logout_button.addEventListener('click', logout);
 
 welcome_message.appendChild(logout_button);
-
+// Here we check to see if the user has a valid login token if its undefined we send them a back button and an error message
 if (user_token === undefined) {
     var back_button = document.createElement('button');
     back_button.innerText = "Back to Login";
     back_button.addEventListener('click', logout);
     error_message.appendChild(back_button);
     error_message.innerText = "Oops! Please Go back and login"
-
+    // If the user has a valid login token, we make an api call that calls our get_colors_success function to display data on the page for the user
 } else {
 
 
